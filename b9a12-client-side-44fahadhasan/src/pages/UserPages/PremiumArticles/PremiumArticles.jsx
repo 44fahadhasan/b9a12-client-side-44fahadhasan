1b21f3135fd234c4fa96dedeac53ac47de322673
Fahad Hasan <44fahadhasan@gmail.com>
2024-06-07 00:34:54 +0600

    Premium-Articles working..

--- b9a12-client-side-44fahadhasan/src/pages/UserPages/PremiumArticles/PremiumArticles.jsx
+++ b9a12-client-side-44fahadhasan/src/pages/UserPages/PremiumArticles/PremiumArticles.jsx
@@ -1,5 +1,51 @@
+import { useQuery } from "@tanstack/react-query";
+import ContainerBox from "../../../components/ContainerBox/ContainerBox";
+import SectionContent from "../../../components/SectionContent/SectionContent";
+import useAuth from "../../../hooks/useAuth";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import PremiumArticleCard from "./PremiumArticleCard/PremiumArticleCard";
+
 const PremiumArticles = () => {
-  return <div>PremiumArticles</div>;
+  const { user } = useAuth();
+
+  const axiosSecure = useAxiosSecure();
+
+  const { data: premiumArticles = [] } = useQuery({
+    queryKey: ["premiumArticles"],
+    enabled: !!user?.email,
+    queryFn: async () => {
+      const res = await axiosSecure.get("/premium-articles", {
+        headers: { email: user?.email },
+      });
+      return res?.data;
+    },
+  });
+
+  console.log(premiumArticles);
+
+  return (
+    <div className="bg-[#E6E7E8] font-[sans-serif]">
+      <ContainerBox>
+        {/* card area */}
+        <div>
+          <div className="text-center">
+            <SectionContent title={"Showed All Premium Articles"} />
+          </div>
+
+          {/* card container */}
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mt-16  mx-auto">
+            {/* single card */}
+            {premiumArticles?.map((premiumArticle) => (
+              <PremiumArticleCard
+                key={premiumArticle?._id}
+                premiumArticle={premiumArticle}
+              />
+            ))}
+          </div>
+        </div>
+      </ContainerBox>
+    </div>
+  );
 };
 
 export default PremiumArticles;
